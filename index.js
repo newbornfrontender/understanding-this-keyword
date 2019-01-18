2e73f4c6a8b1a35e84911dc21216015a6bcec1b3
Infant Frontender <newbornfrontender@gmail.com>
2019-01-18 20:49:19 +0300

    Add name method

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ class NameField {
 class NameGenerator {
   constructor() {
     const btn = document.querySelector('button');
-    btn.addEventListener('click');
+    btn.addEventListener('click', this.addName);
   }
 
   addName() {
